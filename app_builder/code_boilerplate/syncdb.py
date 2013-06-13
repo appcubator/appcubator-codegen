e4b13801d3d98a08c55885ae6edeab980c4a9064
Karan Sikka <kssworld93@gmail.com>
2013-06-12 23:51:32 -0700

    syncdb script fixed

--- app_builder/code_boilerplate/syncdb.py
+++ app_builder/code_boilerplate/syncdb.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 import os
 import sys
+import subprocess
+import shlex
 
 if __name__ == "__main__":
-    os.environ["DJANGO_SETTINGS_MODULE"]
+    os.environ["DJANGO_SETTINGS_MODULE"] = 'settings'
+    APP_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
     commands = []
     commands.append('python manage.py syncdb --noinput')
 
-    if not os.path.isdir(os.path.join(app.app_dir, 'webapp', 'migrations')):
-        sys.stdout.write("Web app has not yet been migrated - converting to south.")
+    if not os.path.isdir(os.path.join(APP_DIR, 'webapp', 'migrations')):
+        sys.stdout.write("\nWeb app has not yet been migrated - converting to south.")
         commands.append('python manage.py convert_to_south webapp')
     else:
         commands.append('python manage.py schemamigration webapp --auto')
@@ -19,10 +22,10 @@ if __name__ == "__main__":
     for c in commands:
         print("Running `{}`".format(c))
         try:
-            log_msg = subprocess.check_output(shlex.split(c), env=child_env, cwd=app.app_dir)
+            log_msg = subprocess.check_output(shlex.split(c), cwd=APP_DIR)
         except subprocess.CalledProcessError, e:
-            sys.stderr.write(repr(e.cmd) + " returned with exit code of " + str(e.returncode))
-            sys.stderr.write("Command output: " + e.output)
+            sys.stderr.write("\n%r returned with exit code of %s" % (e.cmd, e.returncode))
+            sys.stderr.write("\nCommand output: %s" % e.output)
         else:
-            sys.stdout.write("%r succeeded. Output:\n%s" % (c, log_msg))
+            sys.stdout.write("\n%r succeeded. Output:\n%s" % (c, log_msg))
 
