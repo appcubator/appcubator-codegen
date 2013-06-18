f95ab3884d94515a32736f4b5a83d721c614052f
Karan Sikka <kssworld93@gmail.com>
2013-06-18 01:15:04 -0700

    validate route

--- app_builder/views.py
+++ app_builder/views.py
@@ -1,7 +1,25 @@
 from django.http import HttpResponse
 from tests import master
+from app_builder.analyzer import App, InvalidDict
 
 
 def test(request):
     ret_code, debug_info = master.main()
     return HttpResponse("%s<br><br>%s" % (ret_code, debug_info))
+
+def validate(request):
+    try:
+        app_state = request.POST['app_state']
+    except KeyError:
+        return HttpResponse("Send json string with key 'app_state'", status=400)
+    try:
+        app_state = simplejson.loads(app_state)
+    except Exception:
+        return HttpResponse("Not valid json :(", status=400)
+
+    try:
+        app = App.create_from_dict(app_state)
+    except InvalidDict, e:
+        return HttpResponse(str(e))
+
+    return HttpResponse("")
