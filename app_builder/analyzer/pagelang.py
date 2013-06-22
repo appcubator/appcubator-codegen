8fc6ac7dcb4b6154b99efdb6ed4e1b90e22e84af
Karan Sikka <kssworld93@gmail.com>
2013-06-22 03:06:51 -0700

    beast mode

--- app_builder/analyzer/pagelang.py
+++ app_builder/analyzer/pagelang.py
@@ -37,9 +37,9 @@ class PageLang(object):
 
         datalang_variable_string = ''.join(['%s ' % self.entity_datalang_map[e].to_code(context=context, seed_id=seed_id) for e in self.page.get_tables_from_url()])
         if not template:
-            args_tuple = tuple(datalang_variable_string.split())
+            args_tuple = tuple(['%s.id' % x for x in datalang_variable_string.split()])
             if len(list(args_tuple)) > 0:
-                args_str = ", args=%s" % repr(args_tuple)
+                args_str = ", args=%s" % repr(args_tuple).replace("'", '').replace('"', '') # dumb hack to remove quotation from string to turn it into an identifier. ('tweet.id',) => (tweet.id,)
             else:
                 args_str = ""
             code = "reverse('webapp.pages.%s'%s)" %(self.page._django_view.identifier, args_str)
