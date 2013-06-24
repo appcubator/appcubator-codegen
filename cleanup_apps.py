9a045ca0a53c49b964caf15ffd8c5eb236c18c50
Karan Sikka <kssworld93@gmail.com>
2013-06-24 00:39:49 -0700

    prettier messages

--- cleanup_apps.py
+++ cleanup_apps.py
@@ -5,9 +5,16 @@ import shutil
 
 def clean():
     tempdir = tempfile.gettempdir()
+    count = 0
+    bytecount = 0
     for directory in os.listdir(tempdir):
         if os.path.isdir(os.path.join(tempdir, directory, 'webapp')):
+            count += 1
+            bytecount += os.path.getsize(os.path.join(tempdir, directory))
             shutil.rmtree(os.path.join(tempdir, directory))
 
+    return (count, bytecount)
+
 if __name__ == "__main__":
-    clean()
+    count, bytecount = clean()
+    print "Deleted %d apps, cleared %d bytes" % (count, bytecount)
