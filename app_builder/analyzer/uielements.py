f5021d77b8effbfadcf40ea53668d1d933478182
Nikhil Khadke <nikhil.khadke@gmail.com>
2013-07-12 18:38:56 -0700

    using empty tag

--- app_builder/analyzer/uielements.py
+++ app_builder/analyzer/uielements.py
@@ -785,13 +785,14 @@ class Iterator(DictInited, Hooked):
             inner_htmls.append(uie_html)
         row_wrapper_style_string = 'display:block; position:relative; height:%dpx;' % (self.container_info.row.layout.height * 15)
         row_wrapper = Tag('div', {'style': row_wrapper_style_string}, content=inner_htmls)
+        empty_row_wrapper = Tag('div', {'style': row_wrapper_style_string}, content="List is empty.")
 
         loop_contents = []
         loop_wrapper = Tag('div', {'style': 'position:relative;'}, content=loop_contents)
-        loop_contents.append("{%% if %s %%}" % self._django_query_id)
         loop_contents.append("{%% for obj in %s %%}" % self._django_query_id)
         loop_contents.append(row_wrapper)
+        loop_contents.append("{% empty %}")
+        loop_contents.append(empty_row_wrapper)
         loop_contents.append("{% endfor %}")
-        loop_contents.append("{% endif %}")
         return loop_wrapper
 
