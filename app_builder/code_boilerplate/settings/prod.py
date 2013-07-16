ac955b2de0beed24acf106e340278709b8b297bc
Karan Sikka <kssworld93@gmail.com>
2013-07-16 03:55:12 -0700

    error emailing

--- app_builder/code_boilerplate/settings/prod.py
+++ app_builder/code_boilerplate/settings/prod.py
@@ -40,3 +40,10 @@ LOGGING = {
         },
     }
 }
+# Simple SMTP
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ["SENDGRID_USERNAME"]
+EMAIL_HOST_PASSWORD = os.environ["SENDGRID_PASSWORD"]
+EMAIL_USE_TLS = False
+DEFAULT_FROM_EMAIL = 'team@appcubator.com'
