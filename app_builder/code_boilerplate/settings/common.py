ee2494efdef6d6fa740536631f4e97a42a0ca670
Nikhil Khadke <nikhil.khadke@gmail.com>
2013-07-16 15:25:00 -0700

    Added analytics middleware

--- app_builder/code_boilerplate/settings/common.py
+++ app_builder/code_boilerplate/settings/common.py
@@ -78,7 +78,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'tracking.middleware.VisitorTrackingMiddleware', # Must be under AuthenticationMiddleware
+    'analytics.middleware.VisitorAnalyticsMiddleware', # Must be under AuthenticationMiddleware
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
