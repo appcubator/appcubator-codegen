948debf897737d3872678dafcc29d9669b92dc13
Nikhil Khadke <nikhil.khadke@gmail.com>
2013-07-16 23:31:26 -0700

    Name changes

--- app_builder/code_boilerplate/scripts/compute_analytics.py
+++ app_builder/code_boilerplate/scripts/compute_analytics.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     APP_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
     commands = []
-    commands.append('python scripts/analytics_compute.py')
+    commands.append('python scripts/analytics_cruncher.py')
 
     for c in commands:
         print("Running `{}`".format(c))
