344336aba4ed967e4f1fd73dd510debde54a3a88
Nikhil Khadke <nikhil.khadke@gmail.com>
2013-07-17 12:12:54 -0700

    Removed extraneous print

--- app_builder/code_boilerplate/scripts/compute_analytics.py
+++ app_builder/code_boilerplate/scripts/compute_analytics.py
@@ -12,12 +12,11 @@ if __name__ == "__main__":
     commands.append('python scripts/analytics_cruncher.py')
 
     for c in commands:
-        print("Running `{}`".format(c))
         try:
             log_msg = subprocess.check_output(shlex.split(c), cwd=APP_DIR)
         except subprocess.CalledProcessError, e:
             sys.stderr.write("\n%r returned with exit code of %s" % (e.cmd, e.returncode))
             sys.stderr.write("\nCommand output: %s" % e.output)
         else:
-            sys.stdout.write("\n%r succeeded. Output:\n%s" % (c, log_msg))
+            print log_msg
 
