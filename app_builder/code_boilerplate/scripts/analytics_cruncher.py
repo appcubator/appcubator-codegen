aede03bbdb974b44256471542ec6f11ca480ee23
Nikhil Khadke <nikhil.khadke@gmail.com>
2013-07-23 22:39:27 -0700

    ranges enter analytics

--- app_builder/code_boilerplate/scripts/analytics_cruncher.py
+++ app_builder/code_boilerplate/scripts/analytics_cruncher.py
@@ -10,8 +10,7 @@ sys_list.insert(0, app_dir)
 
 from analytics.models import Visitor
 from collections import Counter
-import requests
-import os, os.path
+from datetime import timedelta, datetime
 import simplejson
 
 def get_total_users():
@@ -36,13 +35,27 @@ def get_total_visitors():
 def get_total_active_visitors():
   return len(Visitor.objects.active())
 
-def get_total_page_views(today):
-  total_page_views = 0
-  for visitor in Visitor.objects.all():
-    total_page_views += visitor.page_views
-  return total_page_views
+def get_total_page_views_list(num_days=7):
+  days_arr = []
+  now = datetime.now()
+  for i in range(num_days):
+    total_page_views = 0
+    day = i+1
+    cutoff = now - timedelta(days=day)
+    visitors = Visitor.objects.filter(last_update__gte=cutoff)
+    for visitor in visitors:
+      total_page_views += visitor.page_views
+    days_arr.append(total_page_views)
+  return days_arr
 
-def get_total_page_views_dict(today):
+def get_total_active_visitors_list(num_days=7):
+  days_arr = []
+  for i in range(num_days):
+    minutes_in_day = (i+1) * 24 * 60
+    days_arr.append(len(Visitor.objects.active(cutoff=minutes_in_day)))
+  return days_arr
+
+def get_total_page_views_dict():
   page_counter = Counter()
   for visitor in Visitor.objects.all():
     page_counter[visitor.url] += visitor.page_views
@@ -54,12 +67,6 @@ def get_total_page_views():
     total_page_views += visitor.page_views
   return total_page_views
 
-def get_total_page_views_dict():
-  page_counter = Counter()
-  for visitor in Visitor.objects.all():
-    page_counter[visitor.url] += visitor.page_views
-  return dict(page_counter)
-
 # Session start, last update
 def get_tracking_analytics():
   json_data = {}
@@ -69,6 +76,8 @@ def get_tracking_analytics():
   json_data['total_active_visitors'] = get_total_active_visitors()
   json_data['total_page_views'] = get_total_page_views()
   json_data['total_page_views_dict'] = get_total_page_views_dict()
+  json_data['active_visitors_range'] = get_total_active_visitors_list()
+  json_data['page_views_range'] = get_total_page_views_list()
   return json_data
 
 def crunch_analytics():
