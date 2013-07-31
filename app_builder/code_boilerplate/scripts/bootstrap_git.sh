287b165f1116b4f2bf9f7df638e5853dedd03563
Nikhil Khadke <nikhil.khadke@gmail.com>
2013-07-30 19:56:26 -0700

    Next changes

--- app_builder/code_boilerplate/scripts/bootstrap_git.sh
+++ app_builder/code_boilerplate/scripts/bootstrap_git.sh
@@ -3,20 +3,19 @@
 # Bare Repository and Current Deployment directory
 
 echo "Starting bootstrap."
-cd /var/www/apps/$1
+cd /var/www/apps/$2
 echo "Changing directory to git-apps/$1"
-cd ../../../git-apps/
+cd ../../git-apps/
 mkdir $1
 cd $1
 echo "Initializing bare git repository."
-#git init --bare
+git init --bare
 echo "Changing directory to $2"
-cd ../../apps/
-cd $2
-#git init
-#git add *.py webapp/
-#git commit -am "Bootstrap Commit."
-#git remote add appcubator v1factory@staging.appcubator.com:/var/www/git-apps/$2/
-#git push appcubator master
+cd /var/www/apps/$2
+git init
+git add *.py webapp/
+git commit -am "Bootstrap Commit."
+git remote add appcubator v1factory@staging.appcubator.com:/var/www/git-apps/$2/
+git push appcubator master
 echo "Pushed code to bare git repository."
 echo "Success!"
