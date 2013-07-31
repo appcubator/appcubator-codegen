32938e257380119d45257bdaadce10172db1fa25
Nikhil Khadke <nikhil.khadke@gmail.com>
2013-07-31 16:47:59 -0700

    Stable

--- app_builder/code_boilerplate/scripts/bootstrap_git.sh
+++ app_builder/code_boilerplate/scripts/bootstrap_git.sh
@@ -11,13 +11,10 @@ cd $1
 echo "Initializing bare git repository."
 git init --bare
 cd ..
-chown -R v1factory:git $1
 echo "Changing directory to $2"
 cd /var/www/apps/$2
 git init
 git add *.py webapp/
 git commit -am "Bootstrap Commit."
-git remote add appcubator v1factory@staging.appcubator.com:/var/www/git-apps/$1/
-git push appcubator master
-echo "Pushed code to bare git repository."
+git remote add appcubator git@staging.appcubator.com:/var/www/git-apps/$1/
 echo "Success!"
