2d1458f13877c8503221cc911c3396836970ea0f
Karan Sikka <kssworld93@gmail.com>
2013-09-06 22:16:29 -0700

    igh

--- analytics/setup.py
+++ analytics/setup.py
@@ -2,17 +2,16 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
-import sys, os
-import analytics
+
 
 setup(
     name='appcubator-analytics',
     version="0.1.0",
     description="Appcubator Analytics",
-    author='Nikhil Khadke @ Appcubator',
+    author='Appcubator',
     author_email='team@appcubator.com',
     package_dir={'analytics': 'analytics'},
     include_package_data=True,
-    url='https://github.com/appcubator/appcubator-codegen',
+    url='https://github.com/appcubator/appcubator-analytics',
     packages=find_packages(),
 )
