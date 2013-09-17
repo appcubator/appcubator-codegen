f02e7b002d32b2bee3cec72831f198fe58d3f89d
Karan Sikka <kssworld93@gmail.com>
2013-09-17 02:56:08 -0400

    yauf

--- app_builder/naming.py
+++ app_builder/naming.py
@@ -144,7 +144,6 @@ class Namespace(object):
         return new_ident
 
     def make_name_safe_and_unique(self, name):
-        name = str(name) # in case it's identifier type
         candidate = make_safe(name)
 
         while candidate in (i.identifier for i in self.used_ids()):
