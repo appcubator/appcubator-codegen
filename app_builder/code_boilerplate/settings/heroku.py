da7d75be1c4647464fceac52731144263f0ee321
Karan Sikka <kssworld93@gmail.com>
2013-10-01 01:44:16 -0400

    aight

--- app_builder/code_boilerplate/settings/heroku.py
+++ app_builder/code_boilerplate/settings/heroku.py
@@ -1,11 +1,8 @@
 from dev import *
 
-
 """
 Heroku environment is still a dev environment.
 """
-
-WSGI_APPLICATION = 'wsgi_heroku.application'
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
